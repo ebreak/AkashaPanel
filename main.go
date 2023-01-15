d8b58c6b8fbd72aa2bc5f325235f53c1abdad9d4
Kininaru <shiftregister233@outlook.com>
2023-01-15 21:51:37 +0800

    mkdir Akasha Panel

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"AkashaPanel/database"
 	_ "AkashaPanel/routers"
+	"os"
 
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	os.Mkdir(".AkashaPanel", os.ModePerm)
+
+	database.Init()
+
 	beego.BConfig.Listen.HTTPPort = 6244
 	beego.Run()
 }
